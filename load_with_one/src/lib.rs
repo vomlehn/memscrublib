fd91e4163016c066ae588365e378e282866abd68
David VomLehn <dvomlehn@gmail.com>
2023-08-07 15:02:14 -0700

    Reorganize to pass cache information to the memory scrubber.

---
+++ load_with_one/src/lib.rs
@@ -0,0 +1,6 @@
+// The entire cache line loads with a single load
+
+use ecc_read;
+
+// The ECC unit operations on 64-bit data
+pub type ECCData = u64;
--- memory_scrubber/src/lib.rs
+++ memory_scrubber/src/lib.rs
@@ -2,38 +2,29 @@
 
 use thiserror::Error;
 
-// ***************
-// Architecture-dependent definitions
-//
-// The following definitions are for the cache with the largest cache line.
-//
-// CACHELINE_WIDTH - Number of bits required to address all bytes in
-//      a cache line.
-// CACHELINE_SIZE - Number of bytes in a cacheline
-// CACHE_WIDTH - Number of bits required to address all cache lines in the
-//      cache
-// CACHE_SIZE - Number of cache lines in the cache
-
-const CACHELINE_WIDTH: usize = 6;
-const CACHELINE_SIZE: usize = 1 << CACHELINE_WIDTH;
-const CACHE_WIDTH: usize = 10;
-const CACHE_SIZE: usize = 1 << CACHE_WIDTH;
-
 // Data type that can hold any address for manipulation as an integer
 type Addr = usize;
 
-// Data type on which the ECC operations. So, if it operates on a 64-but
-// value, this should be u64
-type ECCData = u64;
+// CacheDesc - Description of the cache
+// cacheline_width - Number of bits in the index into the cachline bytes
+// cache_width - Number of bits of the cache index.
+#[derive(Clone, Copy, Debug)]
+pub struct CacheDesc {
+    cacheline_width:    usize,
+    cache_width:        usize,
+}
+
+impl CacheDesc {
+    pub fn cacheline_size(&self) -> usize {
+        1 << self.cacheline_width
+    }
+
+    pub fn cache_size(&self) -> usize {
+        1 << self.cache_width
+    }
+}
+
 //------------------------------------------
-// This mask divides the lower cacheline index and cache index information
-// from the upper bits of an address.
-const addr_mask: usize = !((1 << (CACHELINE_WIDTH + CACHE_WIDTH)) - 1);
-
-const ECCDATA_PER_CACHELINE: usize =
-    CACHELINE_SIZE as usize / std::mem::size_of::<ECCData>();
-type Cacheline = [ECCData; ECCDATA_PER_CACHELINE];
-
 
 // ***************
 
@@ -82,96 +73,83 @@ pub enum MemoryScrubberError {
     ZeroSize,
 }
 
-struct MemoryScrubber {
-    iterator:   MemoryScrubberIterator,
+struct MemoryScrubber<Cacheline> {
+    cache_desc: CacheDesc,
+    start:      *const u8,
+    size:       usize,
+    iterator:   MemoryScrubberIterator<Cacheline>,
 }
 
-impl MemoryScrubber {
+impl<Cacheline> MemoryScrubber<Cacheline> {
 
-    pub fn new(start: *const u8, size: usize) ->
-        Result<MemoryScrubber, MemoryScrubberError> {
-        match MemoryScrubberIterator::new(start, size) {
-            Err(e) => Err(e),
-            Ok(iterator) => Ok(MemoryScrubber {
-                iterator:   iterator,
-            })
-        }
+    pub fn new(cache_desc: &CacheDesc, start: *const u8,
+        size: usize) -> Result<MemoryScrubber<Cacheline>, MemoryScrubberError> {
+        let iterator = MemoryScrubberIterator::new(&cache_desc, start, size)?;
+        Ok(MemoryScrubber {
+            cache_desc: (*cache_desc).clone(),
+            start:      start,
+            size:       size,
+            iterator:   iterator,
+        })
     }
 
     pub fn scrub(&mut self, n: usize) -> Result<(), MemoryScrubberError> {
-        Ok(())
-    }
-}
-
-/*
-    pub fn scrub(&self, n: usize) -> Result<(), MemoryScrubberError> {
-println!("There are {} cache lines", CACHE_SIZE);
-println!("start {:x}", self.start as Addr);
-println!("shift is {}", CACHELINE_WIDTH + CACHE_WIDTH);
-        // Get a mask that will allow extracting the address bits higher
-        // than the cache index
-        let addr_mask = !((1 << (CACHELINE_WIDTH + CACHE_WIDTH)) - 1);
-
-        // Get the highest address less that the starting address that
-        // corresponds to the zero index in the cache.
-        let start_addr = self.start as Addr & addr_mask;
-
-        // Pull out the cache index corresponding the the starting address
-        let start_offset = (self.start as Addr & !addr_mask) >> CACHELINE_WIDTH;
-        println!("mask {:x} masked {:x} start_offset {} 0x{:x}",
-                 addr_mask,
-                 self.start as Addr & addr_mask,
-                 start_offset, start_offset);
-
-        // Scan through each cache line
-        for cache_line in 0..CACHE_SIZE {
-            if cache_line == 2 {
-                break;
-            }
-            let base_offset = cache_line + start_offset;
-
-            // If the first address would be past the end of the memory area,
-            // we're done.
-            if base_offset as isize > self.size {
-                break;
-            }
-
-            let base_addr = start_addr + (base_offset << CACHELINE_WIDTH);
-            println!("base_addr {:x}", base_addr);
+        if (n % self.cache_desc.cacheline_size()) != 0 {
+            return Err(MemoryScrubberError::UnalignedSize);
         }
+
+        for _i in 0..n {
+            let _p = match self.iterator.next() {
+                None => {
+                    self.iterator =
+                        match MemoryScrubberIterator::new(&self.cache_desc,
+                        self.start, self.size) {
+                        Err(e) => return Err(e),
+                        Ok(iterator) => iterator,
+                    };
+                    self.iterator.next().unwrap()
+                },
+                Some(p) => p,
+            };
+        }
+        
         Ok(())
     }
 }
-*/
 
 // Iterator to scan a region of memory, keeping on a single cache line as
 // long as possible.
 //
-// start:   Beginning address of memory, which is aligned on a cache line
-//          boundary
-// size:    Number of cache lines in the area to be scrubbed
-// index:   Value that, when added to the cache index value of start, yields
-//          the index of the cache line being scrubbed
-// offset:  Number of cache lines between the first address corresponding to
-//          the given cache index and the address that will be read. This is
-//          a multiple of the number cache lines in the cache.
-pub struct MemoryScrubberIterator {
-    start:  *const Cacheline,
-    size:   isize,
-    index:  isize,
-    offset: isize,
+// start:       Beginning address of memory, which is aligned on a cache line
+//              boundary
+// size:        Number of cache lines in the area to be scrubbed
+// index:       Value that, when added to the cache index value of start, yields
+//              the index of the cache line being scrubbed
+// offset:      Number of cache lines between the first address corresponding to
+//              the given cache index and the address that will be read. This is
+//              a multiple of the number cache lines in the cache.
+// addr_mask:   This mask divides the lower cacheline index and cache index
+//              information from the upper bits of an address.
+pub struct MemoryScrubberIterator<Cacheline> {
+    cache_desc: CacheDesc,
+    start:      *const Cacheline,
+    size:       isize,
+    index:      isize,
+    offset:     isize,
+    addr_mask:  usize,
 }
 
-impl MemoryScrubberIterator {
+impl<Cacheline> MemoryScrubberIterator<Cacheline> {
+
     // Create a new MemoryScrubber.
     // start: pointer to the beginning of the memory area to be scrubbed. Must
     //      be a multiple of the cache line size.
     // size: number of bytes in the memory area to be scrubbed. Must be a
     //      multiple of the cache line size.
-    pub fn new(start: *const u8, size: usize) ->
-        Result<MemoryScrubberIterator, MemoryScrubberError> {
+    pub fn new(cache_desc: &CacheDesc, start: *const u8, size: usize) ->
+        Result<MemoryScrubberIterator<Cacheline>, MemoryScrubberError> {
         let start_addr = start as Addr;
-        if (start_addr % CACHELINE_SIZE) != 0 {
+        if (start_addr % cache_desc.cacheline_size()) != 0 {
             return Err(MemoryScrubberError::UnalignedAddress);
         }
 
@@ -179,26 +157,30 @@ impl MemoryScrubberIterator {
             return Err(MemoryScrubberError::ZeroSize);
         }
 
-        if (size % CACHELINE_SIZE) != 0 {
+        if (size % cache_desc.cacheline_size()) != 0 {
             return Err(MemoryScrubberError::UnalignedSize);
         }
 
         Ok(MemoryScrubberIterator {
-            start: start as *const Cacheline,
-            size: (size / CACHELINE_SIZE) as isize,
-            index: 0,
-            offset: 0,
+            cache_desc: (*cache_desc).clone(),
+            start:      start as *const Cacheline,
+            size:       (size / cache_desc.cacheline_size()) as isize,
+            index:      0,
+            offset:     0,
+            addr_mask:  !((1 << (cache_desc.cacheline_width +
+                             cache_desc.cache_width)) - 1),
         })
     }
 
     // Return the cache index for the given address
-    fn cache_index(addr: *const Cacheline) -> isize {
+    fn cache_index(&self, addr: *const Cacheline) -> isize {
         // Pull out the cache index corresponding the the starting address
-        ((addr as Addr & !addr_mask) >> CACHELINE_WIDTH) as isize
+        ((addr as Addr & !self.addr_mask) >>
+            self.cache_desc.cacheline_width) as isize
     }
 }
 
-impl Iterator for MemoryScrubberIterator {
+impl<Cacheline> Iterator for MemoryScrubberIterator<Cacheline> {
     type Item = *const Cacheline;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -220,11 +202,11 @@ impl Iterator for MemoryScrubberIterator {
             //
             // To this is added the number of cache lines in the cache. This is
             // the offset, in cache lines, from the starting address.
-            let base_offset = MemoryScrubberIterator::cache_index(self.start) +
+            let base_offset = self.cache_index(self.start) +
                 self.index as isize + self.offset;
 
             if base_offset < self.size {
-                self.offset += CACHE_SIZE as isize;
+                self.offset += self.cache_desc.cache_size() as isize;
                 let res = unsafe {
                     self.start.offset(base_offset)
                 };
@@ -241,27 +223,46 @@ impl Iterator for MemoryScrubberIterator {
 mod tests {
     use super::*;
 
+    // Cache characteristics
+    // CACHELINE_WIDTH - number of bits required to index a byte in a cache line
+    // CACHE_WIDTH - number of bits used as a cache line index in the cache
+    const CACHELINE_WIDTH: usize = 6;
+    const CACHE_WIDTH: usize = 10;
+
+    // Cache descriptor to pass to the memory scrubbing functions.
+    const CACHE_DESC: CacheDesc = CacheDesc {
+        cacheline_width:    CACHELINE_WIDTH,
+        cache_width:        CACHE_WIDTH,
+    };
+
+    // ECCData - The data size used to compute the ECC
+    // Cacheline - the data type of a cache line
+    type ECCData = u64;
+    type Cacheline = [u64; (1 << CACHELINE_WIDTH) / std::mem::size_of::<ECCData>()];
+
+
     #[test]
     fn test_aligned() {
-        let size = CACHELINE_SIZE * CACHE_SIZE * 14;
+        let size = CACHE_DESC.cacheline_size() * CACHE_DESC.cache_size() * 14;
         let mem_area: Vec<u8> = vec![0; size];
         let p = mem_area.as_ptr() as *const u8;
         let (p, size) = align_area(p, size);
 
-        let mut memory_scrubber = match MemoryScrubber::new(p, size) {
-            Err(e) => panic!("Unable to create MemoryScrubberIterator: {}", e),
-            Ok(memory_scrubber) => memory_scrubber,
+        let mut memory_scrubber =
+            match MemoryScrubber::<Cacheline>::new(&CACHE_DESC, p, size) {
+            Err(e) => panic!("MemoryScrubber::new() failed {}", e),
+            Ok(scrubber) => scrubber,
         };
 
-        match memory_scrubber.scrub(CACHELINE_SIZE * 10) {
-            Err(e) => panic!("Scrub failed: {}", e),
-            Ok(_) => println!("Scrub succeeded!"),
+        match memory_scrubber.scrub(CACHE_DESC.cacheline_size() * 10) {
+            Err(e) => panic!("scrub failed: {}", e),
+            Ok(_) => println!("scrub succeeded!"),
         };
     }
 
     #[test]
     fn test_unaligned_address() {
-        let size = CACHELINE_SIZE * 16;
+        let size = CACHE_DESC.cacheline_size() * 16;
         let mem_area: Vec<u8> = vec![0; size];
         let p = mem_area.as_ptr() as *const u8;
         let (p, size) = align_area(p, size);
@@ -269,7 +270,8 @@ mod tests {
             p.offset(1)
         };
 
-        let memory_scrubber = MemoryScrubberIterator::new(p, size);
+        let memory_scrubber =
+            MemoryScrubberIterator::<Cacheline>::new(&CACHE_DESC, p, size);
         assert!(memory_scrubber.is_err());
         assert_eq!(memory_scrubber.err().unwrap(),
             MemoryScrubberError::UnalignedAddress);
@@ -277,13 +279,14 @@ mod tests {
 
     #[test]
     fn test_unaligned_size() {
-        let size = CACHELINE_SIZE * 16;
+        let size = CACHE_DESC.cacheline_size() * 16;
         let mem_area: Vec<u8> = vec![0; size];
         let p = mem_area.as_ptr() as *const u8;
         let (p, size) = align_area(p, size);
         let size = size - 1;
 
-        let memory_scrubber = MemoryScrubberIterator::new(p, size);
+        let memory_scrubber =
+            MemoryScrubberIterator::<Cacheline>::new(&CACHE_DESC, p, size);
         assert!(memory_scrubber.is_err());
         assert_eq!(memory_scrubber.err().unwrap(),
             MemoryScrubberError::UnalignedSize);
@@ -291,25 +294,26 @@ mod tests {
 
     #[test]
     fn test_zero_size() {
-        let size = CACHELINE_SIZE * 16;
+        let size = CACHE_DESC.cacheline_size() * 16;
         let mem_area: Vec<u8> = vec![0; size];
         let p = mem_area.as_ptr() as *const u8;
-        let (p, size) = align_area(p, size);
+        let (p, _size) = align_area(p, size);
         let size = 0;
 
-        let memory_scrubber = MemoryScrubberIterator::new(p, size);
+        let memory_scrubber =
+            MemoryScrubberIterator::<Cacheline>::new(&CACHE_DESC, p, size);
         assert!(memory_scrubber.is_err());
         assert_eq!(memory_scrubber.err().unwrap(),
             MemoryScrubberError::ZeroSize);
     }
 
     fn align_area(p: *const u8, size: usize) -> (*const u8, usize) {
-        let lower_p = (p as Addr) % CACHELINE_SIZE;
-        let p_offset = CACHELINE_SIZE - lower_p;
+        let lower_p = (p as Addr) % CACHE_DESC.cacheline_size();
+        let p_offset = CACHE_DESC.cacheline_size() - lower_p;
         let p = unsafe { p.offset(p_offset as isize) };
 
         let size = size - p_offset;
-        let size = size - (size % CACHELINE_SIZE);
+        let size = size - (size % CACHE_DESC.cacheline_size());
         (p, size)
     }
 }
