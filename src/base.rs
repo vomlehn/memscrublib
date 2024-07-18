975d1791d4a4e43b573dcec3b398cb3ed89f8bf7
David VomLehn <dvomlehn@gmail.com>
2024-07-18 14:26:57 -0700

    Pull things out of lib.rs.

---
+++ src/base.rs
@@ -0,0 +1,463 @@
+// Base traits that can be used to construct non-standard memory scrubbersRun once through the scrubber loop
+extern crate num_traits;
+
+use core::fmt;
+use core::mem;
+//use core::ops::{Add};
+use core::ptr;
+//use num_traits::{PrimInt, Unsigned};
+use num_traits::PrimInt;
+use std::convert::From;
+use std::iter;
+//use std::marker::PhantomData;
+//use std::slice;
+
+/*
+mod crate::addr;
+mod crate::base;
+mod crate::data;
+*/
+/*
+use crate::addr::{Addr, AddrImplTrait};
+use crate::data::DataImplTrait;
+*/
+use crate::addr::*;
+use crate::data::*;
+
+// Error definitions. Because this is core software, it returns errors instread
+// of panicing wherever possible. These are the errors it can return.
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(C)]
+pub enum Error {
+    InternalError,
+    UnalignedStart,
+    UnalignedEnd,
+    UnalignedSize,
+    UnalignedValue,
+    NoMemAreas,
+    EmptyMemArea,
+    ZeroSize,
+    IteratorFailed,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// # Names for generic parameters
+///
+/// | Name | Description |
+/// |---|---|
+/// | D | Integer data type for ECC unit processing. That is, the ECC |
+/// |   | unit processes as many bytes at a time as are in this data type. |
+/// | A |  Integer data type representing the address. If this is usize, it |
+/// |   | should be correct for virtual addresses. Physical addresses may be |
+/// |   | larger. |
+///
+/// # Conventions for naming cache parameters:
+///
+/// | Name | Description |
+/// | N | Number of cache lines |
+/// | S | Number of bytes in a cache line |
+/// | W | Number of ways per cache line |
+
+pub trait AutoScrubDesc<
+    const N: usize,
+    const S: usize,
+    const W: usize,
+    D,
+    A,
+> where
+    D: DataImplTrait<D>,
+    A: AddrImplTrait<A>,
+{
+    fn next(&mut self) -> Addr<A>;
+}
+
+// This is for a generic cache. There is a two-tier implementation where
+// traits define the behavior for most caches but which allows for overriding
+// methods to implement non-classical behavior.
+
+pub trait MemoryScrubberBase<
+    'a,
+    const N: usize,
+    const W: usize,
+    const S: usize,
+    D,
+    A,
+    I,
+> where
+    D: DataImplTrait<D>,
+    A: AddrImplTrait<A>,
+    I: CacheIterator<'a, N, W, S, D, A>,
+    usize: From<A>,
+{
+    /// Returns a reference to a CacheBase trait
+    fn cache(&self) -> &dyn CacheBase<N, W, S, D, A>;
+
+    /// Return a references to an array of MemAreas.
+    fn scrub_areas(&self) -> &[MemArea<A>];
+
+    /// Check that the parameters for the cache make sense
+    ///
+    /// # Arguments:
+    /// * `cache` - Reference to trait CacheBase to check
+    ///
+    /// * `scrub_areas` - Reference to an array of MemAreas being scrubbed
+    ///
+    /// # Returns:
+    /// Either OK(()) or an Error
+    fn check_scrubber_params(
+        cache: &dyn CacheBase<N, W, S, D, A>,
+        scrub_areas: &[MemArea<A>],
+    ) -> Result<(), Error> {
+        // Check the corresponding cache trait
+        CacheBase::<N, W, S, D, A>::check_cache_params(cache)?;
+
+        //  Verify scrub area descriptions
+        if scrub_areas.len() == 0 {
+            return Err(Error::NoMemAreas);
+        }
+
+        let cacheline_size = S;
+        let cacheline_mask: Addr<A> = (cacheline_size - 1).into();
+
+        // Check each scrub area for errors
+        for scrub_area in scrub_areas {
+            let start = scrub_area.start();
+            let end = scrub_area.end();
+
+            if start >= end {
+                return Err(Error::EmptyMemArea);
+            }
+
+            if (start & cacheline_mask) != 0.into() {
+                return Err(Error::UnalignedStart);
+            }
+
+            if (end & cacheline_mask) != cacheline_mask {
+                return Err(Error::UnalignedEnd);
+            }
+        }
+
+        Ok(())
+    }
+
+    /// This is the core of the scrubbing work. We scrub the given number
+    /// of bytes out of the total scrubbing areas supplied, starting after
+    /// the previous location.
+    ///
+    /// # Arguments:
+    ///
+    /// * `n` - Number of bytes to scrub
+    ///
+    /// # Returns:
+    /// Ok(()) or Error
+    fn scrub(&self, n: Addr<A>) -> Result<(), Error>
+    where
+        Self: Sized,
+        usize: From<A>,
+    {
+        let cacheline_size = S;
+        let cacheline_size_mask = (cacheline_size - 1).into();
+        let cacheline_width = CachelineBase::<S, D, A>::cacheline_width();
+
+        // Verify that the number of bytes to scrub is an even multiple of the
+        // cacheline size
+        if (n & cacheline_size_mask) != 0.into() {
+            return Err(Error::UnalignedSize);
+        }
+
+        let n_scrublines = n >> Addr::<A>(cacheline_width.into());
+// FIXME: Add ? operator
+        let iterator =
+            ScrubCountIterator::new(self.cache(), self.scrub_areas(), n_scrublines);
+
+        // At this point, it's pretty much Iterators all the way down.
+        for p in iterator {
+            // FIXME: implement read_cacheline(p);
+            eprintln!("Implemented read_cacheline()");
+            //            Cacheline::<S, D>::read_cacheline(p);
+        }
+
+        Ok(())
+    }
+}
+
+pub trait CacheIterator<
+    'a,
+    const N: usize,
+    const W: usize,
+    const S: usize,
+    D,
+    A,
+>: iter::Iterator
+where
+    D: DataImplTrait<D>,
+    A: AddrImplTrait<A>,
+{
+    fn new(cache: &dyn CacheBase<N, W, S, D, A>, scrub_areas: &[MemArea<A>], n: Addr<A>) -> Self;
+}
+
+// This is the basic definition of cache line data. Note that is is never
+// instantiated, though traits that use this trait usually are. Specifically,
+// means that none these will have "self" parameter.
+pub trait CachelineDataBase<const S: usize, D> {}
+
+pub trait CachelineBase<const S: usize, D, A>
+where
+    D: DataImplTrait<D>,
+    A: AddrImplTrait<A>,
+/*
+    *mut D: From<A>,
+    Addr<A>: Into<*mut D>,
+*/
+{
+    // Check cache line related parameters.
+    //
+    // #Returns:
+    // Ok(()) or Error
+    fn check_cacheline_params() -> Result<(), Error>
+    where
+        Self: Sized,
+    {
+        bit_width::<usize>(std::mem::size_of::<D>())?;
+        bit_width::<usize>(S)?;
+        Ok(())
+    }
+
+    // Return the number of bits required to hold the index into the cache
+    // line
+    fn cacheline_width() -> usize {
+        bit_width::<usize>(std::mem::size_of::<D>() * S).unwrap()
+    }
+
+    // Return the number of bytes in a cache line
+    fn cacheline_size() -> usize
+    where
+        Self: Sized,
+    {
+        1 << Self::cacheline_width()
+    }
+
+    // Return the size of a MemArea in cache lines
+    fn size_in_cachelines(scrub_area: &MemArea<A>) -> Addr<A>
+    where
+        Self: Sized,
+        A: AddrImplTrait<A>,
+    {
+        let cacheline_width: Addr<A> = Self::cacheline_width().into();
+
+        let start = scrub_area.start();
+        let start_in_cachelines = start >> cacheline_width;
+
+        //let x = Addr(32);
+
+        //let x = Addr<u128>(32);
+
+        //let x: Addr::<u128>;
+        //let x = Addr::<u128>::(32);
+        let x = Addr::<u128>(32);
+        let y = Addr::<u128>(4);
+        //let y = Addr::<u128>::new(4);
+        println!("x({}) + y({}) = {}", x, y, x + y);
+        println!("x({}) >> y({}) = {}", x, y, x >> y);
+        println!(
+            "x({}) >> y({}) = {}",
+            start,
+            cacheline_width,
+            start >> cacheline_width
+        );
+
+        // This will truncate the number of cache lines by one
+        let end = scrub_area.end();
+        let end_in_cachelines = end >> cacheline_width;
+
+        (end_in_cachelines - start_in_cachelines) + 1.into()
+    }
+
+    // Read the first element in the cache line indicated by the address
+    fn read(p: Addr<A>) -> D
+    where
+        Self: Sized,
+    {
+        let ptr: *mut D = p.into();
+        let a_ref: &[D; S] = unsafe { &*(ptr as *const _) };
+        unsafe { ptr::read(&a_ref[0]) }
+    }
+
+    // Read the entire cacheline
+    fn read_cacheline(p: Addr<A>)
+    where
+        Self: Sized,
+    {
+        Self::read(p);
+    }
+}
+
+// FIXME: is there any way to drop the CL parameter and define it in terms
+// of N, W, D, and S?
+pub trait CacheBase<const N: usize, const W: usize, const S: usize, D, A>
+where
+    D: DataImplTrait<D>,
+    A: AddrImplTrait<A>,
+    usize: From<A>,
+{
+    // Report on whether any parameter problems are detected
+    fn check_cache_params(&self) -> Result<(), Error> {
+        bit_width::<usize>(N)?;
+        bit_width::<usize>(W)?;
+        Ok(())
+    }
+
+    // Return the number of bits used to index into the cache, i.e. the index
+    // of a cache line in the cache. A cache with 1024 lines will have an
+    // index using 10 bits.
+    fn cache_index_width(&self) -> usize {
+        //println!("CacheBase: cache_index_width: entered");
+        bit_width::<usize>(N).unwrap()
+    }
+
+    /// Computes the offset, in cache lines, of the next address at or higher
+    /// than the given pointer for an address hitting the given cache index.
+    ///
+    /// NOTE: You are unlikely to ever need to implement this
+    ///
+    /// # Attributes
+    /// * `p` - Address to start at
+    /// * `index` - Cache index to search for
+    ///
+    /// # Return value
+    /// The offset to the next address
+    fn offset_to_next_index(&self, p: Addr<A>, index: usize) -> usize
+    where
+        Self: Sized,
+        A: AddrImplTrait<A>,
+    {
+        // FIXME: can I just use Self now?
+        let start_index =
+            <Self as CacheBase<N, W, S, D, A>>::cache_index(self, p);
+        let cache_lines =
+            <Self as CacheBase<N, W, S, D, A>>::cache_lines(self);
+
+        // Compute the offset from the start of the self.scrub_area to the
+        // next highest address whose cache index is the one we are currently
+        // scrubbing.
+        let result = if index >= start_index {
+            index - start_index
+        } else {
+            (index + cache_lines) - start_index
+        };
+        result as usize
+    }
+
+    /// Extract the cache index part of the address
+    /// # Parameters
+    /// * `self` - Self
+    /// * `p` - Address from which the cache index is to be extracted
+    /// NOTE: You are unlikely to ever need to implement this
+    /// # Return value
+    /// The cache line index
+    fn cache_index(&self, p: Addr<A>) -> usize
+    where
+        Self: Sized,
+        A: AddrImplTrait<A>,
+    {
+        let cacheline_width_usize =
+            CachelineBase::<S, D, A>::cacheline_width();
+        let cacheline_width: Addr<A> = cacheline_width_usize.into();
+        // FIXME: can I just use Self now?
+        let cache_index_width =
+            <Self as CacheBase<N, W, S, D, A>>::cache_index_width(self);
+        let shifted_index = p >> cacheline_width;
+        //        let shifted_index_usize: usize = shifted_index.into();
+        let shifted_index_usize: usize = usize::from(shifted_index.into());
+        shifted_index_usize & ((1 << cache_index_width) - 1)
+    }
+
+    // Return the number of cache lines in the index.
+    //
+    // NOTE: You are unlikely to ever need to implement this
+    fn cache_lines(&self) -> usize {
+        // FIXME: can I just use Self now?
+        1 << <Self as CacheBase<N, W, S, D, A>>::cache_index_width(self)
+    }
+}
+
+/// Structure used to define an area to be scrubbed
+/// * `start` - lowest virtual address of the area. Must be a multiple of the
+///     cache line size
+///
+/// * end - address of the last byte of the area. Must be one less than a
+///     multiple of the cache line size
+#[derive(Clone, Copy, Debug)]
+#[repr(C)]
+pub struct MemArea<A>
+where
+    A: AddrImplTrait<A>,
+{
+    pub s: Addr<A>,
+    pub e: Addr<A>,
+}
+
+impl<A> MemArea<A>
+where
+    A: AddrImplTrait<A>,
+{
+    pub fn new(start: Addr<A>, end: Addr<A>) -> MemArea<A> {
+        MemArea::<A> { s: start, e: end }
+    }
+
+    /// Returns the address of the first byte contained in the memory area
+    ///
+    /// # Returns:
+    /// Addr<A> which is the address of the first byte
+    fn start(&self) -> Addr<A> {
+        self.s
+    }
+
+    /// Returns the address of the last byte contained in the memory area. Note
+    /// that this is inclusive
+    ///
+    /// # Returns:
+    /// Addr<A> which is the address of the last byte
+    fn end(&self) -> Addr<A> {
+        self.e
+    }
+}
+
+/// Compute the minimum number of bits required to hold a given value.
+/// The number must be a non-zero multiple of two.
+///
+/// # Type parameters
+///
+/// * `T` - PrimInt and Debug
+///
+/// # Attributes
+/// * `size` - Type T value whose width is to be computed.
+///
+/// # Return:
+/// * `Error` - If the value is not a multiple of two.
+///
+/// * `usize` - Number of bits required to hold the given value
+pub fn bit_width<T: PrimInt + std::fmt::Debug>(
+    size: T,
+) -> Result<usize, Error> {
+    if size == T::zero() {
+        return Err(Error::ZeroSize);
+    }
+
+    let leading_zeros = size.leading_zeros(); // Type is u32
+    let bits_per_byte = 8; // FIXME: should be T.BITS/size.BITS...
+    let size_in_bytes = mem::size_of::<T>(); // Type is usize
+    let size_in_bits = size_in_bytes * (bits_per_byte as usize);
+    let width = (size_in_bits - 1) - (leading_zeros as usize);
+    if size != (T::one() << width) {
+        return Err(Error::UnalignedValue);
+    }
+
+    Ok(width)
+}
---
+++ src/data.rs
@@ -0,0 +1,327 @@
+// Define things for the struct Data<A>, which allows arbitrary addresses to be
+// computed. To do so, Data<A> implements num_traits::Unsigned and it's possible to
+// add two Data<A> values together. To do this, Data<A> has an element of type
+// A which also implements Unsigned. This allows specifying an underlying integer
+// type to use for performing the operations.
+//
+// FIXME: Make Data be ECCData?
+
+use core::fmt;
+use core::num::ParseIntError;
+use core::ops::{Add, AddAssign, Div, Mul, Rem, Sub, SubAssign};
+use core::ops::{BitAnd, Shl, Shr};
+use num_traits::{Num, One, Unsigned, Zero};
+//use std::convert::From;
+
+// FIXME: rename ECCDataTrait?
+pub trait DataImplTrait<D>:
+    Unsigned + Copy + Into<*mut D> + fmt::Debug
+{
+}
+
+// FIXME: remove allow(dead_code)
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub struct Data<D>(D);
+
+impl<D> DataImplTrait<u32> for Data<D>
+where
+    D: DataImplTrait<D>,
+    *mut u32: From<D>,
+{
+}
+
+impl<D> fmt::Display for Data<D>
+where
+    D: Unsigned + fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<D> fmt::LowerHex for Data<D>
+where
+    D: Unsigned + fmt::LowerHex,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:x}", self.0)
+    }
+}
+
+// From num_traits
+
+impl<D> Unsigned for Data<D> where D: DataImplTrait<D> {}
+
+impl<D> Num for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type FromStrRadixErr = ParseIntError;
+    fn from_str_radix(
+        _: &str,
+        _: u32,
+    ) -> Result<Self, Self::FromStrRadixErr> {
+        // FIXME: impl<D>ement this
+        unimplemented!();
+    }
+}
+
+impl<D> One for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    fn one() -> Self::Output {
+        Data(D::one())
+    }
+}
+
+impl<D> Zero for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    fn zero() -> Self::Output {
+        Data(D::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+/*
+impl<D> From<u32> for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    fn from(value: u32) -> Self {
+        value.into() // Convert u32 to Data
+    }
+}
+*/
+
+impl<D> From<Data<D>> for *mut u32
+where
+    *mut u32: From<D>,
+{
+    fn from(value: Data<D>) -> Self {
+        value.0.into()
+    }
+}
+
+impl<D> From<Data<D>> for usize
+where
+    D: Unsigned,
+    usize: From<D>,
+{
+    fn from(value: Data<D>) -> Self {
+        value.0.into()
+    }
+}
+
+impl<D> From<usize> for Data<D>
+where
+    D: Unsigned + From<usize>,
+{
+    fn from(value: usize) -> Self {
+        value.into() // Convert usize to Data
+    }
+}
+
+impl<D> From<u128> for Data<D>
+where
+    D: Unsigned + From<u128>,
+{
+    fn from(value: u128) -> Self {
+        value.into() // Convert u128 to Data
+    }
+}
+
+impl<D> From<u64> for Data<D>
+where
+    D: Unsigned + From<u64>,
+{
+    fn from(value: u64) -> Self {
+        value.into() // Convert u64 to Data
+    }
+}
+
+impl<D> From<u32> for Data<D>
+where
+    D: Unsigned + From<u32>,
+{
+    fn from(value: u32) -> Self {
+        value.into() // Convert u32 to Data
+    }
+}
+
+/*
+impl<D> From<usize>
+for *mut usize
+where
+    D: DataImplTrait<D>,
+{
+}
+
+impl<D> DataImplTrait<D><usize>
+for usize
+where
+    D: DataImplTrait<D>,
+{
+}
+
+impl<D> DataImplTrait<D><u128>
+for u128
+where
+    D: DataImplTrait<D>,
+{
+}
+
+impl<D> DataImplTrait<D><u64>
+for u64
+where
+    D: DataImplTrait<D>,
+{
+}
+
+impl<D> DataImplTrait<D><u32>
+for u32
+where
+    D: DataImplTrait<D>,
+{
+}
+
+impl<D> Into<*mut u32>
+for u32
+where
+    D: DataImplTrait<D>,
+{
+    fn into(self) -> *mut u32 {
+        self.into()
+    }
+}
+*/
+
+/*
+impl<D><D> From<DataImplTrait<D><D>> for u32
+where
+    D: DataImplTrait<D>,
+where
+    D: Unsigned,
+    usize: From<D>,
+{
+    fn from(value: Addr<D>) -> Self {
+        value.addr.into()
+    }
+}
+*/
+
+// Arithmetic from core::ops
+
+impl<D> Add for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type Output = Data<D>;
+
+    fn add(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 + rhs.0)
+    }
+}
+
+impl<D> AddAssign for Data<D>
+where
+    D: DataImplTrait<D> + AddAssign,
+{
+    fn add_assign(&mut self, rhs: Data<D>) {
+        (*self).0 += rhs.0
+    }
+}
+
+impl<D> Div for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type Output = Data<D>;
+
+    fn div(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 / rhs.0)
+    }
+}
+
+impl<D> Mul for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type Output = Data<D>;
+
+    fn mul(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 * rhs.0)
+    }
+}
+
+impl<D> Rem for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type Output = Data<D>;
+
+    fn rem(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 / rhs.0)
+    }
+}
+
+impl<D> Sub for Data<D>
+where
+    D: DataImplTrait<D>,
+{
+    type Output = Data<D>;
+
+    fn sub(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 - rhs.0)
+    }
+}
+
+impl<D> SubAssign for Data<D>
+where
+    D: DataImplTrait<D> + SubAssign,
+{
+    fn sub_assign(&mut self, rhs: Data<D>) {
+        (*self).0 -= rhs.0
+    }
+}
+
+// Bit operations from core::ops
+
+impl<D> BitAnd for Data<D>
+where
+    D: DataImplTrait<D> + BitAnd<Output = D>,
+{
+    type Output = Data<D>;
+
+    fn bitand(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 & rhs.0)
+    }
+}
+
+impl<D> Shl for Data<D>
+where
+    D: DataImplTrait<D> + Shl<Output = D>,
+{
+    type Output = Data<D>;
+
+    fn shl(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 << rhs.0)
+    }
+}
+
+impl<D> Shr for Data<D>
+where
+    D: DataImplTrait<D> + Shr<Output = D>,
+{
+    type Output = Data<D>;
+
+    fn shr(self, rhs: Data<D>) -> Self::Output {
+        Data(self.0 >> rhs.0)
+    }
+}
